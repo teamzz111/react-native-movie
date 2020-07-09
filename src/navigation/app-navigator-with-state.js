44292fb210a326f126a583f09a637d77253c2364
teamzz111 <felipeflogxd@gmail.com>
2020-07-09 02:06:59 -0500

    App Navigator with state

---
+++ src/navigation/app-navigator-with-state.js
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux'
+import { createReduxContainer } from 'react-navigation-redux-helpers'
+
+import AppNavigator from './app-navigator'
+
+// Listen everything that comes in 'root'
+const ReduxContainer = createReduxContainer(AppNavigator, 'root')
+
+export class AppNavigatorWithState extends ReduxContainer {
+
+}
+
+// Send props from the state
+function mapStateToProps(state) {
+  return {
+    state: state.navigation
+  }
+}
+
+export default connect(mapStateToProps)(AppNavigatorWithState)
