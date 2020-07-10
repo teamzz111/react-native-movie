b9cfe9df1838136c465f1e23a3e81266ac451b0f
teamzz111 <felipeflogxd@gmail.com>
2020-07-10 01:47:12 -0500

    StatusBar

--- src/providers/DarkThemeProdiver.js
+++ src/providers/DarkThemeProdiver.js
@@ -2,12 +2,14 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { ThemeProvider } from "styled-components";
+import { StatusBar } from "react-native";
 
 const DarkThemeProvider = ({ children }) => {
   const darkThemeEnabled = useSelector((state) => state.preferences.darkThemeEnabled, []);
   
   return (
     <ThemeProvider theme={{ theme: darkThemeEnabled ? "dark" : "light" }}>
+      <StatusBar backgroundColor = {darkThemeEnabled ? 'black' : 'white'} barStyle = {darkThemeEnabled ? 'light-content' : 'dark-content'} />
       {children}
     </ThemeProvider>
   );
