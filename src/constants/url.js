8aa544274fe7e53e69c6cf0f409b92e4c4d45c83
teamzz111 <felipeflogxd@gmail.com>
2020-07-10 12:22:18 -0500

    Add details constanst

--- src/constants/url.js
+++ src/constants/url.js
@@ -8,4 +8,9 @@ export const moviesList = {
   list: 'movie/popular',
   image: 't/p/w200',
   rated: 'movie/top_rated'
-}
+};
+
+export const movieDetail = {
+  detail: 'movie/',
+  big: 't/p/w500'
+};
