6fed70b33c308b7397a4574e53a43ff1d55d3897
teamzz111 <felipeflogxd@gmail.com>
2020-07-10 02:44:00 -0500

    Re factor navigation

--- src/navigation/app-navigator.js
+++ src/navigation/app-navigator.js
@@ -1,51 +1,17 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import Loading from '../screens/loading';
 import { createStackNavigator } from 'react-navigation-stack';
-import { useSelector, useDispatch } from "react-redux";
-import theme from 'styled-theming';
-import styled from 'styled-components/native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 
-export const backgroundColor = theme('theme', {
-  light: '#fff',
-  dark: '#2d2d2d',
-});
-
-export const textColor = theme('theme', {
-  light: '#000',
-  dark: '#fff',
-});
-
-const Container = styled.View`
-  flex: 1;
-  justifyContent: center;
-  alignContent: center;
-  alignItems: center;
-  backgroundColor: ${backgroundColor};
-  color: ${textColor};
-`;
-
-function HomeScreen() {
-  const dispatch = useDispatch();
-
-  return (
-    <Container>
-        <TouchableOpacity onPress = {() => dispatch({ type: 'TOGGLE_DARKTHEME' })}>
-          <Text>asd</Text>
-        </TouchableOpacity>
-    </Container>
-  );
-}
+import HomeScreen from '../screens/home';
 
 const AppNavigator = createStackNavigator(
   {
     App: HomeScreen,
-    Auth: HomeScreen,
-    Loading,
   },
   {
     initialRouteName: 'App', // default - Loading
+    defaultNavigationOptions: {
+    }
   },
 );
 
