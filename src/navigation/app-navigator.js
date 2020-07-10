f6eac495ffcd2b919148edf603114bbe714cc709
teamzz111 <felipeflogxd@gmail.com>
2020-07-10 12:22:24 -0500

    Add new screen to navigator

--- src/navigation/app-navigator.js
+++ src/navigation/app-navigator.js
@@ -1,13 +1,15 @@
 import { createStackNavigator } from 'react-navigation-stack';
 
 import HomeScreen from '../screens/home';
+import Detail from '../screens/detail';
 
 const AppNavigator = createStackNavigator(
   {
     App: HomeScreen,
+    Detail
   },
   {
-    initialRouteName: 'App',
+    initialRouteName: 'Detail',
     defaultNavigationOptions: {
       headerShown: false
     }
