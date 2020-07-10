6d6b96c667f8c19ff67c676855c03fa6f680bfa0
teamzz111 <felipeflogxd@gmail.com>
2020-07-10 01:47:02 -0500

    StyledComponent

--- src/components/loading/styledView.js
+++ src/components/loading/styledView.js
@@ -2,7 +2,6 @@ import styled from 'styled-components/native';
 
 export const View = styled.View `
     flex: 1;
-    alignItems: center;
     backgroundColor: #FF5722;
     justifyContent: center;
 `;
